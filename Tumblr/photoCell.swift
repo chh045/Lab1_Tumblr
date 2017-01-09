0e1e76b4f52f47a8aa5e8c9e6e9d70e1fd0896f7
Chuanqiao Huang <edison111us@gmail.com>
2017-01-09 00:37:38 -0800

    first commit

---
+++ Tumblr/photoCell.swift
@@ -0,0 +1,24 @@
+//
+//  photoCell.swift
+//  Tumblr
+//
+//  Created by Huang Edison on 1/8/17.
+//  Copyright © 2017 Edison. All rights reserved.
+//
+
+import UIKit
+
+class photoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
